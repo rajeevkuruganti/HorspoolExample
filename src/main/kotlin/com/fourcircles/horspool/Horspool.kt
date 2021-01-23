b66f3e297caa07c1fe744009875c32080e2786f2
rkkotlin <rkuruganti@4circlesllc.com>
2021-01-22 19:09:14 -0500

    removed unused code

--- src/main/kotlin/com/fourcircles/horspool/Horspool.kt
+++ src/main/kotlin/com/fourcircles/horspool/Horspool.kt
@@ -1,26 +1,9 @@
 package com.fourcircles.horspool
 
 class Horspool {
-    fun shifttable(pattern: String) {
-        var i: Int
-        var j: Int
-        val m: Int
-        val p = pattern.toCharArray()
-        m = pattern.length
-        i = 0
-        while (i < SIZE) {
-            table[i] = m
-            i++
-        }
-        j = 0
-        while (j < m) {
-            table[p[j].toInt()] = m - 1 - j
-            j++
-        }
-    }
 
- fun boyerMooreHorspoolSearch(source: String, pattern: String): Int
-    {
+
+    fun boyerMooreHorspoolSearch(source: String, pattern: String): Int {
 
         val patt = pattern.toCharArray()
         val patternLength: Int = patt.size
@@ -48,20 +31,5 @@ class Horspool {
             i += shift[src[i + patternLength - 1].toInt()]
         }
         return -1
-    }
-    companion object {
-        var SIZE = 500
-        var table = IntArray(SIZE)
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val pos: Int
-//            val source = "The cow jumped over the moon. The horse jumped over the fence."
-            val source = "Thecowjumpedoverthemoon"
-            val pattern = "jumledove"
-            val horspoolInstance = Horspool()
-            horspoolInstance.shifttable(pattern)
-            pos = horspoolInstance.boyerMooreHorspoolSearch(source, pattern)
-            if (pos == -1) println("PATTERN NOT FOUND") else println("PATTERN FOUND FROM POSITION: \t$pos\n")
-        }
     }
 }
