5bb24e36b72bfefa5e2fdb95bb415b656ab68fd3
rkkotlin <rkuruganti@4circlesllc.com>
2021-01-23 17:31:36 -0500

    refactored code and added more tests

--- src/main/kotlin/com/fourcircles/horspool/Horspool.kt
+++ src/main/kotlin/com/fourcircles/horspool/Horspool.kt
@@ -5,8 +5,8 @@ class Horspool {
 
     fun boyerMooreHorspoolSearch(source: String, pattern: String): Int {
 
-        val patt = pattern.toCharArray()
-        val patternLength: Int = patt.size
+        val pattChar = pattern.toCharArray()
+        val patternLength: Int = pattChar.size
         if (patternLength == 0) {
             return 0
         }
@@ -15,14 +15,14 @@ class Horspool {
 
         var shift = IntArray(200) { patternLength }
         for (k in 0..(patternLength - 2)) {
-            shift[patt[k].toInt()] = patternLength - 1 - k
+            shift[pattChar[k].toInt()] = patternLength - 1 - k
         }
 
         var i = 0
         var j: Int
         while ((i + patternLength) <= srcLength) {
             j = patternLength - 1
-            while (source[i + j] == patt[j]) {
+            while (source[i + j] == pattChar[j]) {
                 j -= 1
                 if (j < 0) {
                     return i
--- src/test/kotlin/com/fourcircles/horspool/HorspoolTest.kt
+++ src/test/kotlin/com/fourcircles/horspool/HorspoolTest.kt
@@ -8,22 +8,51 @@ class HorspoolTest() {
     private var horspool: Horspool = Horspool()
 
     @Test
+    fun `no pattern`() {
+        val source = "Test Tooth String. Molar teeth are growing."
+        val pattern = ""
+        val returnValue = horspool.boyerMooreHorspoolSearch(source, pattern)
+        assert(returnValue == 0)
+    }
+
+    @Test
     fun `happy path`() {
         val source = "Test Tooth String. Molar teeth are growing."
         val pattern = "Tooth"
         val returnValue = horspool.boyerMooreHorspoolSearch(source, pattern)
-        println("haoopy " + returnValue)
         assert(returnValue == 5)
-
     }
+
+    @Test
+    fun `Multiple words happy path`() {
+        var source = "Test tooth String. Molar tooth are growing."
+        val pattern = "tooth"
+        val resultArray = mutableListOf<Int>()
+        var start: Int = 0
+        var partialSource = source
+        while (start <= source.length) {
+            val returnValue = horspool.boyerMooreHorspoolSearch(partialSource, pattern)
+            if (returnValue != -1){
+                resultArray.add(returnValue+start)
+                start = start + returnValue + pattern.length
+                partialSource = source.substring(start,source.length)
+            } else {
+                break
+            }
+        }
+        assert(resultArray.size == 2)
+    }
+
+
+
+
 // Test not hpapy path
     @Test
     fun `not happy path`(){
         val source ="Test Tooth String"
         val pattern = "Molar Teeth"
         var returnValue = horspool.boyerMooreHorspoolSearch(source, pattern)
-        println("Not happy Path = $returnValue")
-
+        assert(returnValue == -1)
     }
 
 }
